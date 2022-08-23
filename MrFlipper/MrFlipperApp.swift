20af80264adace1ae4e5421f324b7be25be71ec0
dmnhat91 <henry.duong02@gmail.com>
2022-08-23 20:01:49 +0700

    Initial Commit

---
+++ MrFlipper/MrFlipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrFlipperApp.swift
+//  MrFlipper
+//
+//  Created by Duong Minh Nhat on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MrFlipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
